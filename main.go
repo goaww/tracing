963f75c1a6bb4c2b27ef94631b322e595ed987b5
Hai Nguyen <haind.vng@gmail.com>
2020-10-22 21:36:03 +0700

    Play with context

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/log"
@@ -11,17 +12,31 @@ func main() {
 	tracer, closer := InitTracer("tracing-service")
 	defer closer.Close()
 
-	span := createMainSpan(tracer)
+	span := createMainSpan(tracer, "hello-span")
 	defer (*span).Finish()
 
 	doStub(span)
 
 	logStub(span)
 
+	// Play with context
+	ctx := context.Background()
+	ctxTracer, ctxCloser := InitTracer("context-service")
+	defer ctxCloser.Close()
+	opentracing.SetGlobalTracer(ctxTracer) //important!!!
+
+	ctxSpan := createMainSpan(ctxTracer, "hello-context-span")
+	defer (*ctxSpan).Finish()
+
+	ctx = opentracing.ContextWithSpan(ctx, *ctxSpan)
+
+	doStubWithContext(&ctx)
+
+	logStubWithContext(&ctx)
 }
 
-func createMainSpan(tracer opentracing.Tracer) *opentracing.Span {
-	span := tracer.StartSpan("hello-span")
+func createMainSpan(tracer opentracing.Tracer, name string) *opentracing.Span {
+	span := tracer.StartSpan(name)
 	fmt.Println("Hello!!!")
 	span.SetTag("http.url", "Localhost")
 	span.SetTag("http.method", "GET")
@@ -45,5 +60,24 @@ func logStub(rootSpan *opentracing.Span) {
 	fmt.Println("Log Stub....")
 
 	span.LogFields(log.String("event", "logStub"), log.Bool("success", false))
+
+}
+
+func doStubWithContext(ctx *context.Context) {
+	span, _ := opentracing.StartSpanFromContext(*ctx, "doStubWithContext")
+	defer span.Finish()
+	fmt.Println("Do Stub with context....")
+
+	span.LogFields(log.String("event", "doStubWithContext"), log.Bool("success", true))
+
+}
+
+func logStubWithContext(ctx *context.Context) {
+	span, _ := opentracing.StartSpanFromContext(*ctx, "logStubWithContext")
+
+	defer span.Finish()
+	fmt.Println("Log Stub with context....")
+
+	span.LogFields(log.String("event", "logStubWithContext"), log.Bool("success", false))
 
 }
